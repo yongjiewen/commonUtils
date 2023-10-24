5599152e0ecbedb1f620649fda973aa1872f2d7f
jack <wenyj@thinkive.com>
2023-10-24 14:20:57 +0800

    feat: 添加vuepress文档说明

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  title: "js工具库",
+  base: "/commonUtils/",
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  themeConfig: {
+    // logo: '/commonUtils/image.png',
+    nav: [
+      {
+        text: "首页",
+        link: "/"
+      },
+      {
+        text: "文档",
+        link: "/docsFile/start"
+      },
+      {
+        text: "Github",
+        link: "https://github.com/yongjiewen/commonUtils"
+      }
+    ],
+    sidebar: {
+      '/docsFile/': [
+        {
+          title: "开始",
+          children: ["/docsFile/start"],
+          initialOpenGroupIndex: 1
+        }
+      ]
+    }
+  }
+};
